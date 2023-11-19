5e8ffbcc3bffe5ed930d2713888a480ce305dc30
Génesis Arteaga <al182129@alumnos.uacj.mx>
2023-11-18 17:42:23 -0700

    Sidebar conexiones y Css pendiente

--- front_empresa/src/app/shared/sidebar/sidebar.component.ts
+++ front_empresa/src/app/shared/sidebar/sidebar.component.ts
@@ -1,28 +1,55 @@
 import { Component } from '@angular/core';
-import {MenuModule} from 'primeng/menu';
+import {MenuItemContent, MenuModule} from 'primeng/menu';
 import {MenuItem} from 'primeng/api';
 import { Router } from '@angular/router';
 import { RegistroService } from 'src/app/services/registro/registro.service';
 import { jwtDecode } from 'jwt-decode';
 import { LoginService } from 'src/app/services/login.service';
 
+
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
+
 export class SidebarComponent {
   mensaje: string = '';
   isAuthenticated: boolean = this.login.isAuthenticated();
    usuario:any;
 
-  itemsUser!: MenuItem[];
-  itemsLibros!: MenuItem[];
+   itemsUser!: MenuItem[];
 
   value: string | undefined;
 
   constructor(private router: Router, usuario:RegistroService, private login:LoginService) { }
-
+  items: any[] = [
+    {
+      label: 'Libros',
+      items: [
+        { label: 'Fantasia', routerLink: '/shojo' },
+        { label: 'Clasicos', routerLink: '/shonen' },
+        { label: 'Romance', routerLink: '/seinen' },
+        { label: 'Hispanohablantes', routerLink: '/seinen' }
+      ]
+    },
+    {
+      label: 'Comics',
+      items: [
+        { label: 'Superhéroes', routerLink: '/superheroes' },
+        { label: 'Comedia', routerLink: '/comedia' },
+        { label: 'Ciencia Ficción', routerLink: '/ciencia-ficcion' }
+      ]
+    },
+    {
+      label: 'Libros',
+      items: [
+        { label: 'Ficción', routerLink: '/ficcion' },
+        { label: 'No Ficción', routerLink: '/no-ficcion' },
+        { label: 'Misterio', routerLink: '/misterio' }
+      ]
+    },
+  ];
   ngOnInit() {
 
     // this.getUserInfo();
@@ -52,30 +79,18 @@ export class SidebarComponent {
         }
       }
     ]
-    this.itemsLibros = [
-      {label: 'Fantasia'},
-      {label:'Romance',command: () => this.navegar('/mi-balance')},
-      {label:'Clasicos'},
-      {label:'Fantasia'},
-     
-    ]
-  
   }
 
 
-  items: any[] = [
-    { label: 'Libros',  routerLink: '/clasicos-libros' 
-  },
-    { label: 'Comics',  routerLink: '/comedia-comics' },
-    { label: 'Mangas',  routerLink: '/shojo' }
-  ];
+
+  
+  
 
   private navegar(ruta: string): void {
     this.router.navigate([ruta]);
   }
   // getUserInfo(): any {
   //   console.log('inicio formulario');
-    
   //   const accessToken = localStorage.getItem('access_token');
 
   //   if (accessToken) {
@@ -83,7 +98,6 @@ export class SidebarComponent {
   //       const decodedToken = jwtDecode(accessToken);
   //       console.log(decodedToken);
   //       this.usuario = decodedToken;
-        
   //       return decodedToken;
   //     } catch (error) {
   //       console.error('Error al decodificar el token:', error);
