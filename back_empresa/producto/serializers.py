cce5c691cb7f4599880e653eb88e450fa442f5e7
Génesis Arteaga <al182129@alumnos.uacj.mx>
2023-11-20 21:26:50 -0700

    Revert "Revert "Merge branch 'develop' of https://github.com/metodosformalesds/t2_mfds_2023 into develop""
    This reverts commit 66721ce8ed06c1a99fa61175f039ffdb9fbe3510.

--- back_empresa/producto/serializers.py
+++ back_empresa/producto/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Producto
+from .models import Producto, Categoria, Genero
 
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
+        fields = '__all__'
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+class GeneroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genero
         fields = '__all__'
--- front_empresa/src/app/services/producto/producto.service.ts
+++ front_empresa/src/app/services/producto/producto.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +14,33 @@ export class ProductoService {
   getProducts(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}producto/productos/`);
   }
+
+  joyotas(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}producto/toptres/`).pipe(
+      map(products => {
+        // Mapea las rutas de las imágenes para que incluyan la URL base
+        return products.map(product => {
+          return {
+            ...product,
+            imagen: `${this.apiUrl}${product.imagen}`,
+          };
+        });
+      })
+    );
+  }
+
+  postProducto(productoForm: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}producto/productos/`, 
+      productoForm     
+    );
+  }
+  
+
+  getCategoria(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}producto/categoria/`);
+  }
+  getGenero(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}producto/generos/`);
+  }
+
 }
