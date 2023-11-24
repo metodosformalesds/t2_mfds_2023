f1a5cb5b958c52c94337e06184bc047df9852b87
Génesis Arteaga <al182129@alumnos.uacj.mx>
2023-11-23 19:02:18 -0700

    Usuario Front

--- front_empresa/src/app/paginas/usuario/usuario.component.ts
+++ front_empresa/src/app/paginas/usuario/usuario.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-usuario',
-  templateUrl: './usuario.component.html',
-  styleUrls: ['./usuario.component.scss']
+ selector: 'app-usuario',
+ templateUrl: './usuario.component.html',
+ styleUrls: ['./usuario.component.scss']
 })
-export class UsuarioComponent {
+export class UsuarioComponent implements OnInit {
+ fotoPerfil = 'assets/imagenes/Login/perfil.webp';
+ fotoPortada = 'assets/imagenes/Login/portada.jpeg';
+
+ constructor() { }
+
+ ngOnInit(): void {
+ }
 }
